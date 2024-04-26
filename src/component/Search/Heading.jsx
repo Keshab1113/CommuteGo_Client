b4270484a66b970274d92234657c5bc2a3e4481a
Keshab Das <110785343+Keshab1113@users.noreply.github.com>
2024-04-27 01:28:13 +0530

    Update Heading.jsx

--- src/component/Search/Heading.jsx
+++ src/component/Search/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import video from '/header.mp4'
 const Heading = () => {
     return (
-        <div className="  w-full h-screen flex justify-center items-center sm:flex-row flex-col-reverse"><video src={video} muted autoPlay loop className=" object-cover w-full h-screen" />
+        <div className="  w-full h-screen flex justify-center items-center sm:flex-row flex-col-reverse">
                 <div className=' absolute h-full w-full flex flex-col justify-center items-center'>
                     <h1 data-aos="fade-up" className="sm:text-6xl text-4xl font-extrabold cursor-default text-violet-50">
                         CommuteGo
